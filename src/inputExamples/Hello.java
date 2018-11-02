2d022136890fad4cf849bfa8e1085c8804c154db
Unknown <ryan.p.osterberg@gmail.com>
2018-11-02 13:21:32 -0500

    Just playing

--- src/inputExamples/Hello.java
+++ src/inputExamples/Hello.java
@@ -4,7 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int age;
+		System.out.println(age=100);
+		System.out.println(age);
 	}
 
 }
